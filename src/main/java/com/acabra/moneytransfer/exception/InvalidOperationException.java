86235d01694b5a918eb6296802ad8849ed5e97cf
Agustin Cabra Sarmiento <sarmiea@amazon.com>
2019-04-29 19:06:47 +0200

    removing unused exception, all exception specifics have replaced this generic one.

--- src/main/java/com/acabra/moneytransfer/exception/InvalidOperationException.java
+++
@@ -1,7 +0,0 @@
-package com.acabra.moneytransfer.exception;
-
-public class InvalidOperationException extends RuntimeException {
-    public InvalidOperationException(String message) {
-        super(message);
-    }
-}
