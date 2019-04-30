34315752d91b27518f4b6bcea21caf6dc964ea8e
Agustin Cabra Sarmiento <sarmiea@amazon.com>
2019-04-30 13:25:46 +0200

    Custom Serialization/Deserialization, attempting to solve consolidation of JSON objects for BigDecimal
    1. Created a custom deserializer for class CreateAccountRequestDTO
    2. Created a custom serializer for class AccountDTO
    
    Ongoing investigation

--- src/main/java/com/acabra/moneytransfer/utils/JsonHelper.java
+++ src/main/java/com/acabra/moneytransfer/utils/JsonHelper.java
@@ -1,7 +1,10 @@
 package com.acabra.moneytransfer.utils;
 
+import com.acabra.moneytransfer.dto.AccountDTO;
+import com.acabra.moneytransfer.dto.CreateAccountRequestDTO;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import java.io.IOException;
@@ -15,7 +18,11 @@ public class JsonHelper {
     public JsonHelper(){
         mapper = new ObjectMapper()
                 .registerModule(new JavaTimeModule())
-                .registerModule(new Jdk8Module());
+                .registerModule(new Jdk8Module())
+                .registerModule(new SimpleModule(){{
+                    addSerializer(AccountDTO.class, new CustomJsonParser.AccountDTOJsonHelper());
+                    addDeserializer(CreateAccountRequestDTO.class, new CustomJsonParser.CreateAccountRequestDTOJsonHelper());
+                }});
     }
 
     public static JsonHelper getInstance() {
